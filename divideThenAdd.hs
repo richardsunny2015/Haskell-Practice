d69ebe1f46cbd7de780df5f75fb327cfa1257011
richardsunny2015 <richardsunny2015@gmail.com>
2018-09-15 13:29:20 -0400

    fix(divideThenAdd): correct type declaration in line 3

--- divideThenAdd.hs
+++ divideThenAdd.hs
@@ -1,4 +1,7 @@
 module DivideThenAdd where
 
-divideThenAdd :: Num a => a -> a -> a
+divideThenAdd :: Fractional a => a -> a -> a
 divideThenAdd x y = (x / y) + 1
+
+subtractThenAdd :: Num a => a -> a -> a
+subtractThenAdd x y = (x - y) + 1
