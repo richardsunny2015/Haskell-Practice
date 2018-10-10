76c642fc055c4fc1ebfabb6d96ebab2befd72c9f
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-10 14:18:23 -0400

    create(babyFunction): Write circumference functions for types Float and Double

--- babyFunction.hs
+++ babyFunction.hs
@@ -18,3 +18,9 @@ addThree x y z = x + y + z
 
 factorial :: Integer -> Integer
 factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
