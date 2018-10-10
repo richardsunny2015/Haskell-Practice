668abbdec3ccfdf86ef989f2500e06c440fe1d5a
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-10 14:13:28 -0400

    create(babyFunction): Write factorial function

--- babyFunction.hs
+++ babyFunction.hs
@@ -15,3 +15,6 @@ removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
