e2d48da1a3516e4bee89f98adcfad867efa9bce5
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-11 21:13:56 -0400

    create(patternMatching): Write bmiTell and other functions using patternMatching

--- patternMatching.hs
+++ patternMatching.hs
@@ -19,3 +19,90 @@ charName 'a' = "Albert"
 charName 'b' = "Broseph"
 charName 'c' = "Cecil"
 charName c = "Joe Schmo"
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+-- addVectors a b = (fst a + fst b, snd a + snd b) <- without pattern matching
+addVectors (a1, a2) (b1, b2) = (a1 + b1, a2 + b2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call 'head' on empty list"
+head' (x : _) = x
+
+tell :: (Show a) => [a] -> String 
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x
+                                                                 ++ " and "
+                                                                 ++ show y
+
+badAdd :: (Num a) => [a] -> a
+badAdd (x:y:z:[]) = x + y + z
+
+betterAdd :: (Num a) => [a] -> a
+betterAdd [] = 0
+betterAdd xs@(x:_) = x + betterAdd (tail xs)
+
+firstLetter :: String -> String
+firstLetter "" = "Empty string, whoops!"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: Double -> String
+bmiTell bmi
+  | bmi <= 18.5 = "You're underweight, eat more!"
+  | bmi <= 25.0 = "Looking good!"
+  | bmi <= 30.0 = "You're overwight. Let's work out together!"
+  | otherwise = "You're obese. Go see a doctor."
+
+bmiTell' :: Double -> Double -> String
+bmiTell' weight height
+  | bmi <= 18.5 = "You're underweight, eat more!"
+  | bmi <= 25.0 = "Looking good!"
+  | bmi <= 30.0 = "You're overwight. Let's work out together!"
+  | otherwise = "You're obese. Go see a doctor."
+  where bmi = weight / height ^ 2
+
+bmiTell'' :: Double -> Double -> String
+bmiTell'' weight height
+  | bmi <= skinny = "You're underweight, eat more!"
+  | bmi <= normal = "Looking good!"
+  | bmi <= overweight = "You're overwight. Let's work out together!"
+  | otherwise = "You're obese. Go see a doctor."
+  where bmi = weight / height ^ 2
+        (skinny, normal, overweight) = (18.5, 25.0, 30.0)
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+  | a <= b    = b
+  | otherwise = a
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+  | a == b    = EQ
+  | a < b    = LT
+  | otherwise = GT
+
+niceGreeting :: String
+niceGreeting = "Hello! So very nice to see you,"
+
+badGreeting :: String
+badGreeting = "oh! Pfft. It's you, "
+
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juan!"
+greet "Fernando" = niceGreeting ++ " Fernando!"
+greet name = badGreeting ++ " " ++ name
+
+initials :: String -> String -> String
+initials firstName lastName = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstName
+          (l:_) = lastName
