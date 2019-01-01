545571b23be54427802b30c54c6ac4c5cb65f64f
richardsunny2015 <richardsunny2015@gmail.com>
2019-01-01 15:20:55 -0500

    create(chapter-2): Create factorial function and f function

--- algorithms-a-functional-approach/chapter-2.hs
+++ algorithms-a-functional-approach/chapter-2.hs
@@ -1,2 +1,11 @@
 module ChapterTwo where
 
+fact 0 = 1
+fact 1 = 1
+fact n
+    | n < 0     = -1
+    | otherwise = n * fact (n - 1)
+
+f l = reverse (f' l [])
+    where f' [] r = r
+          f' (x:xs) r = (2 * x) : (f' xs r)
